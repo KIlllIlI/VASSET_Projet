6e723eb623fb085fbd756bc80addb721defc7c7d
Brandon <brandon.mouillot.caudron@etud.u-picardie.fr>
2025-10-27 18:45:55 +0100

    Programmation de la descente des blocs

--- src/main/java/com/upvj/latrix/HelloApplication.java
+++ src/main/java/com/upvj/latrix/HelloApplication.java
@@ -2,6 +2,8 @@ package com.upvj.latrix;
 
 import com.upvj.latrix.graphicObjects.GameCanvas;
 import com.upvj.latrix.graphicObjects.RectangleLabel;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Group;
@@ -10,11 +12,13 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import com.upvj.latrix.gameObjects.*;
+import javafx.util.Duration;
 
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class HelloApplication extends Application
+{
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -25,54 +29,72 @@ public class HelloApplication extends Application {
         final GameCanvas canvas = new GameCanvas(s);
         //final MenuCanvas menu = new MenuCanvas(s);
 
-
-
         root.getChildren().add(canvas);
         //root.getChildren().add(menu);
-
-
-
-
-
-
-
         stage.setTitle("Latrix!");
         stage.setScene(s);
         stage.show();
 
-        Tetris blok = new Tetris();
-        canvas.InsertInRenderList(blok);
-
-        for (int i=1;i<=3;i++) {
+        for (int i=1;i<=3;i++)
+        {
             RectangleLabel Test = new RectangleLabel(canvas);
 
             Test.setSize(0,0);
             Test.setSizeOffset(150,50);
-
-
             Test.setPosition(0.5,0.5+i*0.08);
-
             Test.setAnchor(0.5,0.5);
-
             Test.setOffset(0,0);
-
             canvas.InsertInRenderList(Test);
-
-
         }
-
         canvas.setBackgroundColor(Color.WHITE);
-
         canvas.StartRender();
 
 
-
-
-
-
+        //Execution du jeu
+        ExecuteGame(canvas);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
+    }
+
+    public static void ExecuteGame(GameCanvas canvas)
+    {
+        boolean isInExecution = true;
+        int fallSpeed = 1000;
+
+        Tetris block = new Tetris();
+
+        canvas.InsertInRenderList(block);
+
+        //Récuperation du bloc par son getter
+        Integer[][] currentBlock = block.getRandomBlockType();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(fallSpeed), event -> {
+            //Faire descendre le bloc avec un vecteur
+            block.moveDownBlock(currentBlock, block.getShapeMatrisMap());
+        }));
+//        do{
+//            try {
+//                //Récuperation de la map et du bloc par les getter
+//                Integer[][] currentBlock = block.getRandomBlockType();
+//                Integer[][] currentMap = block.getShapeMatrisMap();
+//
+//                //Faire descendre le bloc avec un vecteur
+//                block.moveDownBlock(currentBlock, currentMap);
+//
+//
+//                //On attend à la fin de la boucle
+//                Thread.sleep(fallSpeed);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        } while (isInExecution == false);
+        //Créer une boucle while do avec le mouvement vers le bas
+
+        //répéter indifiniment
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 }
