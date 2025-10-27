cfa1e5c363986b416be1af132c0a7063ae7d1f6d
Brandon <brandon.mouillot.caudron@etud.u-picardie.fr>
2025-10-27 18:45:55 +0100

    Programmation de la descente des blocs

--- src/main/java/com/upvj/latrix/HelloApplication.java
+++ src/main/java/com/upvj/latrix/HelloApplication.java
@@ -1,6 +1,5 @@
 package com.upvj.latrix;
 
-
 import com.upvj.latrix.graphicObjects.GameCanvas;
 import com.upvj.latrix.graphicObjects.MenuCanvas;
 import com.upvj.latrix.graphicObjects.Rectangles.*;
@@ -47,6 +46,9 @@ public class HelloApplication extends Application {
         Tetris blok = new Tetris();
         gameCanvas.InsertInRenderList(blok);
 
+        for (int i=1;i<=3;i++) {
+            RectangleLabel Test = new RectangleLabel(gameCanvas);
+
 
 
         gameCanvas.setBackgroundColor(Color.WHITE);
@@ -66,13 +68,51 @@ public class HelloApplication extends Application {
 
 
 
-
-
-
-
+        //Execution du jeu
+        ExecuteGame(canvas);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
+    }
+
+    public static void ExecuteGame(GameCanvas canvas)
+    {
+        boolean isInExecution = true;
+        int fallSpeed = 1000;
+
+        Tetris block = new Tetris();
+
+        canvas.InsertInRenderList(block);
+
+        //Récuperation du bloc par son getter
+        Integer[][] currentBlock = block.getRandomBlockType();
+
+        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(fallSpeed), event -> {
+            //Faire descendre le bloc avec un vecteur
+            block.moveDownBlock(currentBlock, block.getShapeMatrisMap());
+        }));
+//        do{
+//            try {
+//                //Récuperation de la map et du bloc par les getter
+//                Integer[][] currentBlock = block.getRandomBlockType();
+//                Integer[][] currentMap = block.getShapeMatrisMap();
+//
+//                //Faire descendre le bloc avec un vecteur
+//                block.moveDownBlock(currentBlock, currentMap);
+//
+//
+//                //On attend à la fin de la boucle
+//                Thread.sleep(fallSpeed);
+//            } catch (InterruptedException e) {
+//                throw new RuntimeException(e);
+//            }
+//        } while (isInExecution == false);
+        //Créer une boucle while do avec le mouvement vers le bas
+
+        //répéter indifiniment
+        timeline.setCycleCount(Timeline.INDEFINITE);
+        timeline.play();
     }
 }
--- src/main/java/com/upvj/latrix/gameObjects/Tetris.java
+++ src/main/java/com/upvj/latrix/gameObjects/Tetris.java
@@ -65,58 +65,213 @@ public class Tetris implements GraphicObject {
     };
 
     //Apparition d'un bloc au hasard
-    Random random = new Random();
-    Integer[][] randomBlockType = ALL_BLOCKS[random.nextInt(ALL_BLOCKS.length)];
+//    Random random = new Random();
+    Integer[][] randomBlockType;
+
+    final int raw = 20;
+    final int col = 10;
+//    Integer[][] map = new Integer[raw][col];
 
     //définition de la map
-    private Integer[][] ShapeMatrisMap = {
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+    private final Integer[][] ShapeMatrisMap = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
     };
 
-    public Tetris() {
+    private int blockX = 3;
+    private int blockY = 0;
+
+    int[] vector = {0, 1};
+//    bX += vector[0];
+//    bY += vector[1];
+
+    //Constructeur constituant les bases d'un bloc
+    public Tetris()
+    {
         String imagePath = "/com/upvj/latrix/block.png";
         URL imageUrl = Tetris.class.getResource("/com/upvj/latrix/block.png");
         URL image2 = Tetris.class.getResource("/com/upvj/latrix/Logo.png");
-        if (image2 != null) {
+        if (image2 != null)
+        {
             Sprite2 = new Image(image2.toExternalForm());
         } else {
             System.err.println("Error: block.png not found in resources/com/upvj/latrix/");
         }
-        if (imageUrl != null) {
+        if (imageUrl != null)
+        {
             Sprite = new Image(imageUrl.toExternalForm());
         } else {
             System.err.println("Error: block.png not found in resources/com/upvj/latrix/");
         }
+
+        //Apparition d'un bloc
+        spawnNewBlock();
     }
 
     @Override
-    public boolean draw(GraphicsContext gc) {
+    public boolean draw(GraphicsContext gc)
+    {
+        final int blockSize = 40;
 
-        if (Sprite != null)
+        if (Sprite == null)
         {
-            int y = 40;
-            for(Integer[] lineBloc : this.randomBlockType) {
-                int x = 40;
-                for(Integer bloc : lineBloc) {
-                    if (bloc == 1) {
-                        gc.drawImage(Sprite, x, y, 40, 40);
-                    } else if (bloc == 2) {
-                        gc.drawImage(Sprite2, x, y, 40, 40);
+            return false;
+        }
+
+        if (ShapeMatrisMap != null)
+        {
+            for (int i = 0; i < ShapeMatrisMap.length; i++)
+            {
+                for (int j = 0; j < ShapeMatrisMap[i].length; j++)
+                {
+                    if (ShapeMatrisMap[i][j] == 1)
+                    {
+                        gc.drawImage(Sprite, j * blockSize, i * blockSize, blockSize, blockSize);
+                    } else if (ShapeMatrisMap[i][j] == 2) {
+                        gc.drawImage(Sprite2, j * blockSize, i * blockSize, blockSize, blockSize);
                     }
-                    x += 40;
                 }
-                y += 40;
+            }
+        }
+
+
+        if (randomBlockType != null)
+        {
+            int y = blockY * blockSize;
+            for (Integer[] lineBloc : this.randomBlockType)
+            {
+                int x = blockX * blockSize;
+                for (Integer bloc : lineBloc)
+                {
+                    if (bloc == 1)
+                    {
+                        gc.drawImage(Sprite, x, y, blockSize, blockSize);
+                    } else if (bloc == 2 && Sprite2 != null) {
+                        gc.drawImage(Sprite2, x, y, blockSize, blockSize);
+                    }
+                    x += blockSize;
+                }
+                y += blockSize;
             }
         }
         return true;
+    }
+
+    //Méthode de déplacement d'un bloc vers le bas
+    public void moveDownBlock(Integer[][] block, Integer[][] map)
+    {
+        int newY = blockY + 1;
+
+        if (canMoveDown(block, map, newY))
+        {
+            blockY = newY;
+        } else {
+            //le block se fixe sur la carte s'il ne peut plus descendre
+            placeBlock(block, map);
+
+            //fait appraître un nouveau bloc
+            spawnNewBlock();
+        }
+    }
+
+    //Méthode de vérification de si le bloc peut descendre
+    private boolean canMoveDown(Integer[][] block, Integer[][] map, int newY)
+    {
+        for(int i = 0; i < block.length; i++)
+        {
+            for(int j = 0; j < block[i].length; j++)
+            {
+                if(block[i][j] == 1){
+                    int mapY = newY + i;
+                    int mapX = blockX + j;
+
+                    //Si dépasse la dernière ligne
+                    if(mapY >= map.length)
+                    {
+                        return false;
+                    }
+
+                    if (mapX < 0 || mapX >= map[0].length)
+                    {
+                        return false;
+                    }
+
+                    //Si touche un bloc déjà posé
+                    if(map[mapY][mapX] == 1)
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    };
+
+    //Méthode d'immobilisation du bloc sur la map
+    private void placeBlock(Integer[][] block, Integer[][] map)
+    {
+        for (int i = 0; i < block.length; i++)
+        {
+            for (int j = 0; j < block[i].length; j++)
+            {
+                if (block[i][j] == 1)
+                {
+                    int mapX = blockX + j;
+                    int mapY = blockY + i;
+                    map[mapY][mapX] = 1;
+                }
+            }
+        }
+    }
+
+    //Méthode d'apparition d'un bloc aléatoire
+    private void spawnNewBlock()
+    {
+        Random random = new Random();
+        randomBlockType = ALL_BLOCKS[random.nextInt(ALL_BLOCKS.length)];
+        blockX = 3;
+        blockY = 0;
+    }
+
+    //Getter pour la map
+    public Integer[][] getShapeMatrisMap()
+    {
+        return ShapeMatrisMap;
+    }
+
+    //Getter pour le bloc courant
+    public Integer[][] getRandomBlockType()
+    {
+        return randomBlockType;
+    }
+
+    //Getter pour la position X du bloc
+    public int getBlockX()
+    {
+        return blockX;
+    }
+
+    //Getter pour la position Y du bloc
+    public int getBlockY()
+    {
+        return blockY;
     }
 }
